100356218b630401a804909a22cb1817dd9d4ca5
b.arcon <b.arcon@samsung.com>
2018-12-02 21:14:42 +0100

    remote TODO

--- src/components/enums.js
+++ src/components/enums.js
@@ -1,7 +1,7 @@
 export const WEATHER_API = {
   KEY: "199628c7d9e68af528e583a2d6cba205"
 };
-//TODO: remove before push
+
 export const NO_DATA = "No Data";
 
 export const WEEKDAY = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
