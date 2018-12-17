126c1c63cc62c040028aa5d9f2ec5fe0a61b0b97
b.arcon <b.arcon@samsung.com>
2018-12-17 13:09:08 +0100

    add new temporary key

--- src/components/enums.js
+++ src/components/enums.js
@@ -1,7 +1,7 @@
 export const WEATHER_API = {
-  KEY: "199628c7d9e68af528e583a2d6cba205"
+  KEY: "6cb30460aaa2ead9ff4cbae491db5bbc"
 };
 
 export const NO_DATA = "No Data";
 
-export const WEEKDAY = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+export const WEEKDAY = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
